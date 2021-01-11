dacf9aa53af63bcb43811682fd7469aae4e93690
Subhash Chandra <49841848+Subhash703@users.noreply.github.com>
2021-01-11 14:00:34 +0530

    Update Spiral_Print2D_Matrix.cpp
    Now it's valid for any number of rows and columns

--- Spiral_Print2D_Matrix.cpp
+++ Spiral_Print2D_Matrix.cpp
@@ -1,23 +1,58 @@
+/*
+Your Task:
+You dont need to read input or print anything. Complete the function spirallyTraverse() that takes matrix, R and C as input parameters and returns a list of integers denoting the spiral traversal of matrix. 
 
-void spiralMatrix(vector<vector>>&arr){
-	int top = 0, bottom = arr.size()-1, left = 0, right = arr[0].size();
-	
-	while(top<=bottom && left<= right){
-		for(int i = left; i<= right; i++){
-			cout<<arr[top][i]<<" ";
-		}
-		top++;
-		for(int i = top; i<= bottom; i++){
-			cout<<arr[i][right]<<" ";
-		}
-		right--;
-		for(int i = right; i>=left; i--){
-			cout<<arr[bottom][i]<<" ";
-		}
-		bottom--;
-		for(int i = bottom; i>= top; i--){
-			cout<<arr[i][left]<<" ";
-		}
-		left++;
-	}
-}
+Expected Time Complexity: O(R*C)
+Expected Auxiliary Space: O(R*C)
+
+Constraints:
+2 <= R, C <= 100
+0 <= matrixi <= 100
+
+Input:
+R = 3, C = 4  
+matrix[][] = {{1, 2, 3, 4},
+           {5, 6, 7, 8},
+           {9, 10, 11, 12}}
+Output: 
+1 2 3 4 8 12 11 10 9 5 6 7
+Explanation:
+Applying same technique as shown above, 
+output for the 2nd testcase will be 
+1 2 3 4 8 12 11 10 9 5 6 7.
+*/
+
+vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        // code here 
+        vector<int>res;
+        int top = 0, left = 0, right = c-1, bottom = r-1;
+        int size = c*r;
+        while(left <= right && top<=bottom){
+            for(int i = left; i<=right && res.size()<size; i++){
+                // cout<<matrix[left][i]<<" ";
+                res.push_back(matrix[top][i]);
+            }
+            top++;
+            
+            for(int i = top; i<=bottom && res.size()<size; i++){
+                // cout<<matrix[i][right]<<" ";
+                res.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            for(int i = right; i>=left && res.size()<size; i--){
+                // cout<<matrix[bottom][i]<<" ";
+                res.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            
+            for(int i = bottom; i>=top && res.size()<size; i--){
+                // cout<<matrix[i][left]<<" ";
+                res.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+        
+        return res;
+    }
